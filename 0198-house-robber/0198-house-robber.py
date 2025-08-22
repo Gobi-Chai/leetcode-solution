64a88c7eb5e132fc01ee04aa5686c7ba0c0f6cc5
Gobi-Chai <83641181+Gobi-Chai@users.noreply.github.com>
2025-08-22 10:41:01 +0900

    Time: 0 ms (100%), Space: 12.5 MB (54.54%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+
+        if len(nums) == 1:
+            return nums[0]   
+        prev2 = nums[0]
+        prev1 = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            curr = max(prev1, prev2 + nums[i])
+            prev2, prev1 = prev1, curr
+        return prev1
