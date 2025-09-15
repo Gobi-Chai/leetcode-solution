f7adc870a98f861dd615af187cc0257089520ac3
Gobi-Chai <83641181+Gobi-Chai@users.noreply.github.com>
2025-09-15 18:44:57 -0400

    Time: 39 ms (34.4%), Space: 20.4 MB (71.68%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,30 +1,21 @@
-class Solution(object):
-    def subarraySum(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
+from collections import defaultdict
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
         """
         consider pre[i] as the sum of sums[0,...,i] 
         WTS pre[j] - pre[i-1] = k, which is pre[i-1] = pre[j] - k
         we need to iterate the how many times we have got pre[j] - k
     
         """
-
         total = 0 # current prefix sum
-        ans = 0 # numbers of subarrays
-        cnt = {0:1}
+        ans = 0 # nums of subarrays
+        cnt = defaultdict(int)
+        cnt[0] = 1
 
         for x in nums:
-            total += x 
-            need = total - k    
+            total += x
+            need = total - k
             if need in cnt:
                 ans += cnt[need]
-
-            cnt[total] = cnt.get(total, 0) + 1
-
+            cnt[total] += 1
         return ans
-
-
-        
